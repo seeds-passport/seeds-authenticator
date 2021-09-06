1739130c4dd994d78d58a3e4b46553c32935ac90
Hugo Esteves <hugoestevescastro@gmail.com>
2021-09-06 11:50:48 +0100

    Positive and negative tests for /api/v1/new

--- tests/new.rs
+++ tests/new.rs
@@ -1,4 +1,4 @@
-use ureq;
+use ureq::{self, Error};
 use actix_rt;
 use tokio::{self, spawn};
 use authenticator;
@@ -6,8 +6,14 @@ use authenticator::utils::{
 	settings::Settings
 };
 
+
+#[tokio::main]
+async fn spawn_app() {
+    let _ = spawn(authenticator::run().await.expect("Error spawning the API."));
+}
+
 #[actix_rt::test]
-async fn new_works() {
+async fn positive() {
 	let settings = Settings::new().unwrap();
 
     spawn_app();
@@ -20,7 +26,27 @@ async fn new_works() {
     assert_eq!(_resp.status(), 200);
 }
 
-#[tokio::main]
-async fn spawn_app() {
-    let _ = spawn(authenticator::run().await.expect("Error spawning the API."));
+#[actix_rt::test]
+async fn wrong_account_name() {
+
+    spawn_app();
+
+    let _resp = ureq::post("http://127.0.0.1:8080/api/v1/new")
+        .send_json(ureq::json!({
+            "account_name": "wrong-account-name-for-testing"
+        }));
+    let mut status_code: u16 = 0;
+    match _resp {
+        Ok(response) => {
+            status_code = response.status();
+        },
+        Err(Error::Status(code, _)) => {
+            status_code = code;
+        },
+        Err(_) => {
+            // Here for syntax purpose
+            // Never reaches this endpoint
+        }
+    }
+    assert_eq!(status_code, 403);
 }
